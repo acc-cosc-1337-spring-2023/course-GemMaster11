0043216cfef375893a4aa9af484d9ee9f53d9d96
GemMaster11 <123427580+GemMaster11@users.noreply.github.com>
2023-02-14 01:46:34 +0000

    Lecture + Examples 2/13

--- src/examples/04_repetition/01_while/while.cpp
+++ src/examples/04_repetition/01_while/while.cpp
@@ -1,9 +1,25 @@
 #include "while.h"
+
+using std::string;
+using std::cout;
+
 //Using a while loop write code for sum_of_squares function that accepts an int and returns  
 //the sum of all the squares of that number from 1 to the number. 
 //Example: sum_of_squares(4) 
 //1*1 + 2 * 2 + 3*3 + 4*4 = 30
+int sum_of_squares(int num)
+{
+    auto sum = 0;
 
+
+    while(num > 0)
+    {
+        sum += num * num; // sum = num * num + sum;
+        num--; // num -= 1, num == num - 1 
+    }
+
+    return sum;
+}
 
 //Using a while loop write code for display function that accepts a string and displays 
 //each character of the string in a line
@@ -19,3 +35,13 @@ l
 
 */
 
+void display(std::string str)
+{
+    auto index = 0;
+
+    while(index < str.length())
+    {
+        cout<<str[index]<<"\n";
+        index++; //increments 1 for the current value of index
+    }
+}
--- src/examples/04_repetition/03_do_while/do_while.cpp
+++ src/examples/04_repetition/03_do_while/do_while.cpp
@@ -1,7 +1,19 @@
 #include "do_while.h"
+using std::cin;
+using std::cout;
 
 //Write code for void function prompt_user to loop until
 //user opts not to continue.  
+void prompt_user()
+{
+    auto choice = 'y'; //apostrophe represents a character, "" quotation mark represents a string
 
+    do
+    {
+        cout<<"Enter y to continue, any other key to exit ";
+        cin>>choice;
+    }
+    while(choice == 'y' || choice == 'Y');
+}
 
 
